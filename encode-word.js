fc07a32b58ce9b4179c4c567e4618c51f1da80f3
Steve Maya <steve.maya@codeimmersives.com>
2020-11-05 09:53:22 -0600

    commit

--- encode-word.js
+++ encode-word.js
@@ -4,11 +4,18 @@ const emojis = require('./emojis.js');
 // string-building version
 const encodeWord = function(word) {
     let result = '';
+    let found = false;
     for (const char of word) {
         for (const emoji of emojis) {
-            if (emoji.letter === char) {
+            if (emoji.letter === char.toLowerCase()) {
                 result += emoji.symbol;
+                found = true;
             }
+        } if (found === false) {
+            result += char;
         }
     }
-}
+    return result;
+};
+
+module.exports = encodeWord;
--- main.js
+++ main.js
@@ -2,8 +2,16 @@ const translateWord = require('./translate-word.js');
 const encodeWord = require('./encode-word.js');
 
 
-const words = process.argv.slice(2);
-// const encoding = words.map(encodeWord);
-// console.log(encoding.join(' '));
+const words = process.argv.slice(3);
+const encoding = words.map(encodeWord);
 const translation = words.map(translateWord)
-console.log(translation.join(' '));
+const command = process.argv[2];
+
+switch(command) {
+    case 'translate':
+        console.log(translation.join(' '));
+    case 'encode':
+        console.log(encoding.join(' '));
+    default:
+        console.log('Write translate or encode after node main.js to depict what transformation you want.');  
+    }
